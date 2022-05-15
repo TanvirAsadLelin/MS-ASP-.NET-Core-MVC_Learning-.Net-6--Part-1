2cf497a951e16cb3dbd89b3bd99c6fa2a149b8e3
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-15 12:20:29 +0600

    DotNet 6 learn part and theory part start

---
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

acbc94dc437564e190457da069060a4d36c15be8
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-16 23:21:21 +0600

    DotNet 6 learn part add entity framework add data and get data to Category list

---
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using LearnASP.NETCoreMVC_DotNet_6_WebApp.Data;
+using LearnASP.NETCoreMVC_DotNet_6_WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _Db;
+
+        public CategoryController(ApplicationDbContext Db)
+        {
+            _Db = Db;   
+        }
+        public IActionResult Index()
+        {   
+            //var objCategoryList = _Db.Categories.ToList();  
+            IEnumerable<Category> objCategoryList = _Db.Categories;
+             return View(objCategoryList);
+        }
+    }
+}

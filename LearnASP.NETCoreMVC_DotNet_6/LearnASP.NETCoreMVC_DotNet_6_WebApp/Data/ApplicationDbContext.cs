fa9ae6bc8b474eb42d0e43834958205839e470d4
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-16 19:46:34 +0600

    DotNet 6 learn part and theory part 1

---
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+using LearnASP.NETCoreMVC_DotNet_6_WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; } 
+    }
+}
--- LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Program.cs
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Program.cs
@@ -1,7 +1,14 @@
+using LearnASP.NETCoreMVC_DotNet_6_WebApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(
+    builder.Configuration.GetConnectionString("")
+
+    ));
 
 var app = builder.Build();
 

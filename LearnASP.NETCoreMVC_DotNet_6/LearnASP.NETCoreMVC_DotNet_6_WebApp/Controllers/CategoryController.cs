60d2d380058b53e64b153ac7fb0fb73de93d2e19
TanvirAsadLelin <md.tanvirasad.lelin@gmail.com>
2022-05-18 11:35:45 +0600

    Edit category option completed by using enity framework .net 6

--- LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Controllers/CategoryController.cs
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Controllers/CategoryController.cs
@@ -47,5 +47,45 @@ namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Controllers
             }
          return View(obj);
         }
+
+
+        //Get method
+
+        public IActionResult Edit(int? id )
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _Db.Categories.Find(id);
+            //var categoryFromDbFirst = _Db.Categories.FirstOrDefault(u=>u.Id == id);
+           // var categoryFromDbSingle = _Db.Categories.SingleOrDefault(u=>u.Id == id);
+
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+
+        //Post
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("CustomError", "The Display order is not exactly same as name!");
+            }
+            if (ModelState.IsValid)
+            {
+                _Db.Categories.Add(obj);
+                _Db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Models/Category.cs
+++ LearnASP.NETCoreMVC_DotNet_6/LearnASP.NETCoreMVC_DotNet_6_WebApp/Models/Category.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Models
 {
@@ -6,7 +7,10 @@ namespace LearnASP.NETCoreMVC_DotNet_6_WebApp.Models
     {   
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order range 1 to 100 only!")]
         public int DisplayOrder { get; set; }
         public DateTime CreateDateTime { get; set; } = DateTime.Now;
     }
